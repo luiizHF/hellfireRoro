8ce51b7eb4c8bb1697d986cc1938bedfd9bf28b2
luiizHF <luiiz0828@gmail.com>
2017-05-11 13:08:24 -0600

    iniciamos el curso de spring mvc de cero a maestro

---
+++ src/main/java/com/luiizhf/HellfireRoroApplication.java
@@ -0,0 +1,12 @@
+package com.luiizhf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellfireRoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellfireRoroApplication.class, args);
+	}
+}
